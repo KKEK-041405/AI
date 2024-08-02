0e9b61788dd3804a40d94f62161709183ec32c6b
Konatham komal eshwara kumar <komaleshwarakumarkonatham@gmail.com>
2024-08-02 09:54:44 +0530

    Create tic.py

---
+++ tic.py
@@ -0,0 +1,146 @@
+board = [0,2,2,2,2,2,2,2,2,2]0
+def make2():
+    if board[5] == 2: return 5
+    #check for non corner elements
+    for i in [2,4,6,8]:
+        if board[i] == 2: return i
+def check(n,m,o,traget):
+    if board[n] * board[m] * board[o] == traget:
+        if board[n] == 2: return n
+        if board[m] == 2: return m
+        if board[o] == 2: return o
+    return False
+
+def go(n,player):
+    board[n] = player
+
+def possWin(player):
+    traget = 0
+    if player == 5:
+        traget = 50
+    else:
+        traget = 18
+    
+    #horizontal check
+    for i in range(3):
+        product = 1
+        for j in range(3):
+            product *= board[(i*3) + j +1]
+        print(product)
+        if product == traget:
+            for j in range(3):
+                print(board[(i*3) + j +1] == 2)
+                if board[(i*3) + j +1] == 2: return (i*3) + j +1
+    
+    #vertical check
+    for i in range(3):
+        product = 1
+        for j in range(3):
+            product *= board[(j*3) + i +1]
+        if product == traget:
+            for j in range(3):
+                if board[(i*3) + j +1] == 2: return (i*3) + j +1
+    #diagonal check
+    if board[1] * board[5] * board[9] == traget:
+        if board[1] == 2:return 1
+        if board[5] == 2:return 5
+        if board[9] == 2:return 9
+    if board[3] * board[5] * board[7] == traget:
+        if board[3] == 2:return 3
+        if board[5] == 2:return 5
+        if board[7] == 2:return 7
+    return 0
+def Computer(i,mark):
+    if i == 1: go(5,mark)
+    if i == 2:
+        if board[5] == 2:
+            go(5,mark)
+        else:
+            go(1,mark)
+    if i == 3:
+        if board[9] == 2:
+            go(9,mark)
+        else:
+            go(3,mark)
+    if i == 4:
+        if(not possWin(15/mark) == 0): go(possWin(15/mark),mark)
+        else: go(make2,mark)
+    if i == 5:
+        if(not possWin(mark) == 0): go(possWin(mark),mark)#winning move
+        elif(not possWin(15/mark) == 0): go(possWin(15/mark),mark)#block opponet
+        elif(board[7] == 2): go(7,mark)
+        else:go(3,mark)
+    if i == 6:
+        if(not possWin(mark) == 0): go(possWin(mark),mark)#winning move
+        elif(not possWin(15/mark) == 0): go(possWin(15/mark),mark)#block opponet
+        else: go(make2(),mark)
+    if i == 7:
+        if(not possWin(mark) == 0): go(possWin(mark),mark)#winning move
+        elif(not possWin(15/mark) == 0): go(possWin(15/mark),mark)#block opponet
+        else: go(make2(),mark)
+    if i == 8:
+        if(not possWin(mark) == 0): go(possWin(mark),mark)#winning move
+        elif(not possWin(15/mark) == 0): go(possWin(15/mark),mark)#block opponet
+        else: go(make2(),mark)
+    if i == 9:
+        if(not possWin(mark) == 0): go(possWin(mark),mark)#winning move
+        elif(not possWin(15/mark) == 0): go(possWin(15/mark),mark)#block opponet
+        else: go(make2(),mark)
+
+def printBoard():
+    for i in range(3):
+        for j in range(3):
+            if board[(i*3) + j +1] == 3:
+                print("O",end="|")
+            elif board[(i*3) + j +1] == 5:
+                print("X",end="|")
+            else:
+                print(" ",end="|")
+        print("")
+
+def checkWin(player):
+    traget = 0
+    if player == 5:
+        traget = 125
+    else:
+        traget = 27
+    
+    #horizontal check
+    for i in range(3):
+        product = 1
+        for j in range(3):
+            product *= board[(i*3) + j +1]
+        if product == traget:
+            return True    
+    #vertical check
+    for i in range(3):
+        product = 1
+        for j in range(3):
+            product *= board[(j*3) + i +1]
+        if product == traget:
+            return True
+    #diagonal check
+    if board[1] * board[5] * board[9] == traget:
+        return True
+    if board[3] * board[5] * board[7] == traget:
+        return True
+    return False
+def Game():
+    print()
+    c =  int(input("enter 1 to play frist or enter 0 to play second"))
+    for i in range(1,10):
+        print(i%2 == 0)
+        if (i%2) ==  0:
+            Computer(i,5)
+            printBoard()
+            if checkWin(5):
+                print("computer won")
+                break
+        else:
+            go(int(input()),3)
+            printBoard()
+            if checkWin(3):
+                print("human won")
+                break
+        
+Game()
