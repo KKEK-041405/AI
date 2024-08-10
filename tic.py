b0a9cdcdd36021f62e8c557305325e8ce0054e83
KKEK0414 <konathamkomal0414@gmail.com>
2024-08-10 03:17:22 +0000

    update UI

--- tic.py
+++ tic.py
@@ -1,17 +1,16 @@
-board = [0,2,2,2,2,2,2,2,2,2]0
+board = [0,2,2,2,2,2,2,2,2,2]
+
 def make2():
     if board[5] == 2: return 5
     #check for non corner elements
     for i in [2,4,6,8]:
         if board[i] == 2: return i
-def check(n,m,o,traget):
-    if board[n] * board[m] * board[o] == traget:
-        if board[n] == 2: return n
-        if board[m] == 2: return m
-        if board[o] == 2: return o
-    return False
 
 def go(n,player):
+    if player == 5:
+        print("AI decided to place at pos: ",n)
+    else:
+        print("you decided to place mark at: ",n)
     board[n] = player
 
 def possWin(player):
@@ -26,10 +25,8 @@ def possWin(player):
         product = 1
         for j in range(3):
             product *= board[(i*3) + j +1]
-        print(product)
         if product == traget:
             for j in range(3):
-                print(board[(i*3) + j +1] == 2)
                 if board[(i*3) + j +1] == 2: return (i*3) + j +1
     
     #vertical check
@@ -50,6 +47,7 @@ def possWin(player):
         if board[5] == 2:return 5
         if board[7] == 2:return 7
     return 0
+
 def Computer(i,mark):
     if i == 1: go(5,mark)
     if i == 2:
@@ -70,19 +68,7 @@ def Computer(i,mark):
         elif(not possWin(15/mark) == 0): go(possWin(15/mark),mark)#block opponet
         elif(board[7] == 2): go(7,mark)
         else:go(3,mark)
-    if i == 6:
-        if(not possWin(mark) == 0): go(possWin(mark),mark)#winning move
-        elif(not possWin(15/mark) == 0): go(possWin(15/mark),mark)#block opponet
-        else: go(make2(),mark)
-    if i == 7:
-        if(not possWin(mark) == 0): go(possWin(mark),mark)#winning move
-        elif(not possWin(15/mark) == 0): go(possWin(15/mark),mark)#block opponet
-        else: go(make2(),mark)
-    if i == 8:
-        if(not possWin(mark) == 0): go(possWin(mark),mark)#winning move
-        elif(not possWin(15/mark) == 0): go(possWin(15/mark),mark)#block opponet
-        else: go(make2(),mark)
-    if i == 9:
+    if i == 6 or i == 7 or i == 8 or i == 9:
         if(not possWin(mark) == 0): go(possWin(mark),mark)#winning move
         elif(not possWin(15/mark) == 0): go(possWin(15/mark),mark)#block opponet
         else: go(make2(),mark)
@@ -127,20 +113,24 @@ def checkWin(player):
     return False
 def Game():
     print()
-    c =  int(input("enter 1 to play frist or enter 0 to play second"))
+    c =  int(input("enter 1 to play frist or enter 0 to play second: "))
+    printBoard()
     for i in range(1,10):
-        print(i%2 == 0)
         if (i%2) ==  0:
             Computer(i,5)
             printBoard()
             if checkWin(5):
                 print("computer won")
-                break
+                return
         else:
-            go(int(input()),3)
+            pos = int(input("where you want to place the mark: "))
+            while(board[pos] != 2):
+                printBoard()
+                pos = int(input("enter a valid pos: "))
+            go(pos,3)
             printBoard()
             if checkWin(3):
                 print("human won")
-                break
-        
+                return
+    print("It is a TIE!")
 Game()
